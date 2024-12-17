c86f713106a1775b6274edf0227043287a345fcf
ferdousibegumtuli <ferdousibegum1108@gmail.com>
2024-12-18 02:29:38 +0600

    fead:frontend

---
+++ resources/views/frontend/layouts/header.blade.php
@@ -0,0 +1,70 @@
+<html lang="en-us">
+
+<head>
+  <meta charset="utf-8">
+  <title>Reporter - HTML Blog Template</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
+  <meta name="description" content="This is meta description">
+  <meta name="author" content="Themefisher">
+  <link rel="shortcut icon" href="/images/frontend/favicon.png" type="image/x-icon">
+  <link rel="icon" href="/images/frontend/favicon.png" type="image/x-icon">
+  <meta name="theme-name" content="reporter">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+  <link href="https://fonts.googleapis.com/css2?family=Neuton:wght@700&amp;family=Work+Sans:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/css/frontend/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/frontend/style.css">
+</head>
+
+<body>
+
+  <header class="navigation">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light px-0">
+        <a class="navbar-brand order-1 py-0" href="index.html">
+          <img loading="prelaod" decoding="async" class="img-fluid" src="/images/frontend/logo.png" alt="Reporter Hugo">
+        </a>
+        <div class="navbar-actions order-3 ml-0 ml-md-4">
+          <button aria-label="navbar toggler" class="navbar-toggler border-0" type="button" data-toggle="collapse"
+            data-target="#navigation"> <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+
+        <form action="#!" class="search order-lg-3 order-md-2 order-3 ml-auto">
+          <input id="search-query" name="s" type="search" placeholder="Search..." autocomplete="off">
+        </form>
+
+        <div class="collapse navbar-collapse text-center order-lg-2 order-4" id="navigation">
+          <ul class="navbar-nav mx-auto mt-3 mt-lg-0">
+            <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Category
+              </a>
+              <div class="dropdown-menu"> <a class="dropdown-item" href="travel.html">Travel</a>
+                <a class="dropdown-item" href="travel.html">Lifestyle</a>
+                <a class="dropdown-item" href="travel.html">Cruises</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Tag
+              </a>
+              <div class="dropdown-menu"> <a class="dropdown-item" href="travel.html">Travel</a>
+                <a class="dropdown-item" href="travel.html">Lifestyle</a>
+                <a class="dropdown-item" href="travel.html">Cruises</a>
+              </div>
+            </li>
+            <li class="nav-item"> <a class="nav-link" href="contact.html">Sign In</a>
+            </li>
+          </ul>
+          <!-- <div class="col-6" style="text-align: right;">
+            <button type="button" style="color: black; padding: 4" class="btn btn-primary waves-effect waves-light">Sign In</button>
+          </div> -->
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <main>
+    <section class="section">
+      <div class="container">
